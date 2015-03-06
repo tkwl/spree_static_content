ab3682f84755dd2b95f12c75eed3a0503cf24b0a
j540 <james@tokyowheel.com>
2015-03-07 01:31:40 +0700

    update for bootstrap

--- app/overrides/add_pages_to_admin_configuration_sidebar.rb
+++ app/overrides/add_pages_to_admin_configuration_sidebar.rb
@@ -1,5 +1,7 @@
-Deface::Override.new(virtual_path:  "spree/admin/shared/_configuration_menu",
+Deface::Override.new(virtual_path:  "spree/layouts/admin",
                      name:          "pages_admin_configurations_menu",
-                     insert_bottom: "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
-                     text:          "<%= configurations_sidebar_menu_item Spree.t(:pages), admin_pages_path %>",
+                     insert_bottom: "#main-sidebar",
+                     text:          "<% if Spree.user_class && can?(:admin, Spree::Page) %><ul class='nav nav-sidebar'><%= tab Spree.t(:PAGES), url: admin_pages_path, icon: 'file' %></ul><% end %>",
                      disabled:      false)
+
+
